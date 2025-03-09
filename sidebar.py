8e5261d4e68b1ce72e6340c7287c6ae9949a6e4f
chumoath <chumoath.guo@gmail.com>
2025-03-09 21:36:03 +0800

    toc.md生成到指定目录

--- sidebar.py
+++ sidebar.py
@@ -28,8 +28,7 @@ def walk_one_directory(directory, directory_prefix, level):
             sidebar_content.append(buf_string)
 
 
-def main():
-    docs_dir = input("输入文档根目录: ")
+def main(docs_dir):
     if not os.path.exists(docs_dir):
         print(docs_dir + ": Not found")
 
@@ -44,13 +43,21 @@ def main():
         print(docs_dir + ": README.md is not a file.")
 
     for item in os.listdir(docs_dir):
+        if operator.eq(item, ".git"):
+            continue
         item_path = os.path.join(docs_dir, item)
         if os.path.isdir(item_path):
             walk_one_directory(item_path, os.path.basename(item_path), 0)
 
 
 if __name__ == "__main__":
-    main()
-    with open("sidebar.md", mode="w") as f:
-        for line in sidebar_content:
-            f.write(line)
+    while True:
+        docs_dir = input("输入文档根目录(输入q退出): ")
+        if operator.eq(docs_dir, "q"):
+            break
+        main(docs_dir)
+        with open(os.path.join(docs_dir, "toc.md"), mode="w") as f:
+            for line in sidebar_content:
+                f.write(line)
+
+        print(docs_dir + ": generate toc.md OK.")
